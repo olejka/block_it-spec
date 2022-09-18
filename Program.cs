854b17aeb98836bb70d4a39ad3ef8793775e4f50
otadjiba <oleg.tad.f@gmail.com>
2022-09-18 22:17:04 +0300

    task done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] array1 = new string[4] {"1234", "1567", "2", "computer science"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
